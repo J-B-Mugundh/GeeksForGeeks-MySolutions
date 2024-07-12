a02db6f0f058ebd7cbf2fdd4cca4e620014f1d8a
Mugundh J B <mugundhjb@gmail.com>
2024-07-12 22:22:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  private:
+    void dfs(int r, int c, vector<vector<int>> &vis, vector<vector<int>>& grid, vector<pair<int, int>> &vec, int r0, int c0){
+        vis[r][c] = 1;
+        vec.push_back({r - r0, c - c0});
+        int n = grid.size(), m = grid[0].size();
+        int dr[] = {0, 0, 1, -1};
+        int dc[] = {1, -1, 0, 0};
+        
+        for(int i = 0; i < 4; i++){
+            int nrow = r + dr[i];
+            int ncol = c + dc[i];
+            
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                dfs(nrow, ncol, vis, grid, vec, r0, c0);
+            }
+        }
+    }
+  public:
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        set<vector<pair<int, int>>> st;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(!vis[i][j] && grid[i][j] == 1) {
+                    vector<pair<int, int>> vec;
+                    dfs(i, j, vis, grid, vec, i, j);
+                    st.insert(vec);
+                }
+            }
+        }
+        
+        return st.size();
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.countDistinctIslands(grid) << endl;
+    }
+}
+// } Driver Code Ends
